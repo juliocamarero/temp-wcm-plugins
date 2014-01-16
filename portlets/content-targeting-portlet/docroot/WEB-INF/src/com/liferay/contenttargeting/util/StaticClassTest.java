16dcd6184569726fca98f39eb9847acd79267229
Eduardo Garcia <eduardo.garcia@liferay.com>
2014-01-16 19:00:42 +0100

    Test access to static models

---
+++ portlets/content-targeting-portlet/docroot/WEB-INF/src/com/liferay/contenttargeting/util/StaticClassTest.java
@@ -0,0 +1,12 @@
+package com.liferay.contenttargeting.util;
+
+/**
+ * @author Eduardo Garcia
+ *
+ * This is a test class!
+ */
+public class StaticClassTest {
+
+	public static final String MYSTATICSTRING = "HOLA";
+
+}
